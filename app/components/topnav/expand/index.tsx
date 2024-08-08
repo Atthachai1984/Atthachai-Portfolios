c4144a217fb5124ad263438b6e577f885cbb0b92
James <jamesrwyatt35@gmail.com>
2024-08-08 15:49:24 -0400

    fix href for link issue

--- app/components/topnav/expand/index.tsx
+++ app/components/topnav/expand/index.tsx
@@ -6,49 +6,49 @@ const ExpandNav = () => {
       {/*expand Navigation*/}
       <ul className="text-center text-gray-500 text-sm font-light transition ease-in-out display:none">
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="sapa">SAPATV</a>
+          <a href="/sapa">SAPATV</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="pdflive">PDF LIVE</a>
+          <a href="/pdflive">PDF LIVE</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="splashed">Splashed</a>
+          <a href="/splashed">Splashed</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="gikipedia">Gikipedia</a>
+          <a href="/gikipedia">Gikipedia</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="clarie">Clarie</a>
+          <a href="/clarie">Clarie</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="openrice">OpenRice</a>
+          <a href="/openrice">OpenRice</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="88db">88db</a>
+          <a href="/88db">88db</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="kitchentech">Kitchen Tech</a>
+          <a href="/kitchentech">Kitchen Tech</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="eis">eis</a>
+          <a href="/eis">eis</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="2monkeysfilms">2MonkeysFilms</a>
+          <a href="/2monkeysfilms">2MonkeysFilms</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="leatherocasa">LeatherO Casa</a>
+          <a href="/leatherocasa">LeatherO Casa</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="matthewsfamilyfarms">Matthews Family Farms...</a>
+          <a href="/matthewsfamilyfarms">Matthews Family Farms...</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="infinitybusinessdev">Infinity Business Devel...</a>
+          <a href="/infinitybusinessdev">Infinity Business Devel...</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="babadook">Babadook</a>
+          <a href="/babadook">Babadook</a>
         </li>
         <li className="hover:text-gray-900 duration-300 pb-3 cursor-pointer">
-          <a href="movingcard">Moving Card</a>
+          <a href="/movingcard">Moving Card</a>
         </li>
       </ul>
       <div className="flex justify-center pt-6 pb-20">
